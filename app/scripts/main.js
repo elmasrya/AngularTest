d3a8721855a23a38f7f27cf0e8bea71afd50fec8
Andrew El-Masry <andrewelmasry@gmail.com>
2014-11-22 22:30:10 -0500

    test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,51 @@
-console.log('The Iron Yard Rocks');
+var app = angular.module('PeopleList',['ngRoute']);
+
+app.config( function($routeProvider) {
+
+  $routeProvider.when('/', {
+    templateUrl: 'templates/list-template.html',
+    controller: 'PersonController'
+  });
+
+  $routeProvider.when('/about', {
+    templateUrl: 'templates/about-template.html',
+    controller: 'AboutController'
+  });
+  $routeProvider.otherwise({
+    templateUrl:'templates/other-template.html'
+    controller: 'OtherController'
+  });
+});
+
+app.controller('PersonController', function ($scope) {
+
+
+  $scope.people = [
+    {
+      name: 'Bob',
+      age: 45,
+      test: true
+    },
+
+    {
+      name: 'Tim',
+      age: 31,
+      test: true
+    },
+
+    {
+      name: 'Sam',
+      age: 113,
+      test: true
+    },
+  ];
+  $scope.hello = function (n, i){
+
+    if($scope.people[i].name===('Tim'))
+      alert('Hi there!'+ n + ' and I am number ' + i);
+      alert('Hi there!'+ n + ' and I am number ' + i);
+
+    alert('Hi there!'+ n + ' and I am number ' + i);
+  };
+
+});
