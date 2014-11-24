f7c7ab66a990a6511d253c9d49a683a406b1a5ef
Andrew El-Masry <andrewelmasry@gmail.com>
2014-11-24 00:50:54 -0500

    final

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,25 +1,25 @@
 (function () {
 
 
-  var app = angular.module('HolidayList', ['ngRoute', 'restangular']);
+  var app = angular.module('BookApp', ['ngRoute', 'restangular']);
 
   app.config( function ($routeProvider, RestangularProvider) {
 
-    RestangularProvider.setBaseUrl('http://tiy-atl-fe-server.herokuapp.com/collections/enomosayos');
+    RestangularProvider.setBaseUrl('http://tiy-atl-fe-server.herokuapp.com/collections/');
 
     $routeProvider.when('/', {
       templateUrl: 'templates/home.html',
-      controller: 'GiftsController'
+      controller: 'BooksController'
     });
 
     $routeProvider.when('/single/:id', {
       templateUrl: 'templates/single.html',
-      controller: 'GiftsController'
+      controller: 'BooksController'
     });
 
     $routeProvider.when('/add', {
       templateUrl: 'templates/add.html',
-      controller: 'GiftsController'
+      controller: 'BooksController'
     })
 
   });
@@ -38,56 +38,55 @@
 
 (function () {
 
-  angular.module('HolidayList')
-    .factory('giftsFactory', ['$rootScope', 'Restangular', function ($rootScope, Restangular) {
+  angular.module('BookApp')
+    .factory('booksFactory', ['$rootScope', 'Restangular', function ($rootScope, Restangular) {
 
-      var giftsBase = Restangular.all('holidaylist');
+      var bookStart = Restangular.all('enomosayos');
 
-      function getGifts () {
+      function getBooks () {
         // return $http.get(url);
-        return giftsBase.getList();
+        return bookStart.getList();
       }
 
-      function getGift (id) {
+      function getBook (id) {
         //return $http.get(url + id);
-        return giftsBase.get(id);
+        return bookStart.get(id);
       }
 
-      function addGift (gift) {
-        // $http.post(url, gift).success( function () {
-        //   $rootScope.$broadcast('gift:added');
-        // });
-        giftsBase.post(gift).then( function () {
-          $rootScope.$broadcast('gift:added');
+      function addBook (book) {
+
+        bookStart.post(book).then( function () {
+          $rootScope.$broadcast('book:added');
         });
       }
 
       return {
 
-        getGifts: getGifts,
-        getGift: getGift,
-        addGift: addGift
+        getBooks: getBooks,
+        getBook: getBook,
+        addBook: addBook
 
       };
 
     }]);
 
 }());
+
 (function () {
 
-  angular.module('HolidayList')
-    .controller('GiftsController', 
-      ['giftsFactory', '$scope', '$location', '$rootScope', 
-        function (giftsFactory, $scope, $location, $rootScope) {
+  angular.module('BookApp')
+    .controller('BooksController',
+      ['booksFactory', '$scope', '$location', '$rootScope',
+        function booksFactory, $scope, $location, $rootScope) {
 
-        giftsFactory.getGifts().then( function (results) {
-          $scope.gifts = results;
+        booksFactory.getBooks().then( function (results) {
+          $scope.books = results;
         });
 
-        $scope.addGift = function (gift) {
-          giftsFactory.addGift(gift);
+        $scope.addBook = function (book) {
+          booksFactory.addBook(book);
 
-          $rootScope.$on('gift:added', function () {
+          $rootScope.$on('book:added', function () {
             $location.path('/');
           });
 
